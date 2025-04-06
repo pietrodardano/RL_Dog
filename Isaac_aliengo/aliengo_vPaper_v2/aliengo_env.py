004ba9e48877b08aafd6cf2edfeba95d4df39643
pietrodardano <dardanopietro@libero.it>
2025-04-06 16:47:07 +0200

    renamed and reorganized

---
+++ Isaac_aliengo/aliengo_vPaper_v2/aliengo_env.py
@@ -0,0 +1,148 @@
+import math
+import torch
+
+from isaaclab.envs      import ManagerBasedEnv, ManagerBasedRLEnv, ManagerBasedRLEnvCfg
+from isaaclab.assets    import Articulation, RigidObject, AssetBaseCfg
+from isaaclab.assets    import RigidObjectCfg, ArticulationCfg
+from isaaclab.sensors   import ContactSensor, ContactSensorCfg, RayCasterCfg, patterns
+from isaaclab.scene     import InteractiveSceneCfg
+from isaaclab.terrains  import TerrainImporterCfg
+
+from isaaclab.managers  import ObservationGroupCfg as ObsGroup
+from isaaclab.managers  import ObservationTermCfg as ObsTerm
+from isaaclab.managers  import RewardTermCfg as RewTerm
+from isaaclab.managers  import EventTermCfg as EventTerm
+from isaaclab.managers  import TerminationTermCfg as DoneTerm
+from isaaclab.managers  import SceneEntityCfg
+
+from isaaclab.utils       import configclass
+from isaaclab.utils.noise import AdditiveGaussianNoiseCfg as Gnoise
+from isaaclab.utils.noise import AdditiveUniformNoiseCfg as Unoise
+
+from isaaclab_assets.robots.unitree    import AliengoCFG_Color, AliengoCFG_Black
+
+import isaaclab.sim as sim_utils
+import isaaclab_tasks.manager_based.locomotion.velocity.mdp as mdp
+
+
+global ROUGH_TERRAIN
+global HEIGHT_SCAN
+
+########### SCENE ###########
+
+@configclass
+class SceneCfg(InteractiveSceneCfg):
+    """
+    Scene configuration for the AlienGo environment.
+    """
+    # Terrain
+    ground = AssetBaseCfg(
+        prim_path="/World/ground",
+        spawn=sim_utils.GroundPlaneCfg(size=(100.0, 100.0)),
+    )
+    
+    # Robot
+    robot: ArticulationCfg = AliengoCFG_Black.replace(prim_path="{ENV_REGEX_NS}/Robot")
+    
+    # Lights
+    dome_light = AssetBaseCfg(
+        prim_path="/World/DomeLight",
+        spawn=sim_utils.DomeLightCfg(color=(0.9, 0.9, 0.9), intensity=500.0),
+    )
+    
+    cfg_light_distant = sim_utils.DistantLightCfg(
+        intensity=3000.0,
+        color=(0.75, 0.75, 0.75),
+    )
+    cfg_light_distant.func("/World/lightDistant", cfg_light_distant, translation=(1, 0, 10))
+
+
+########### MDP - RL ###########
+
+
+### OBSERVATIONS ###
+
+@configclass
+class ObsCfg(ObsGroup):
+    """
+    Observation configuration for the AlienGo environment.
+    """
+    class PolicyCfg(ObsGroup):
+        ### Robot State
+        base_lin_pos = ObsTerm(func=mdp.root_pos_w, noise=Unoise(n_min=-0.01, n_max=0.01))      # [m]
+        base_quat_pos = ObsTerm(func=mdp.root_quat_w, noise=Unoise(n_min=-0.02, n_max=0.02))    # [quaternion]
+        base_lin_vel = ObsTerm(func=mdp.root_lin_vel_w, noise=Unoise(n_min=-0.1, n_max=0.1))    # [m/s]
+        base_ang_vel = ObsTerm(func=mdp.root_ang_vel_w, noise=Unoise(n_min=-0.08, n_max=0.08))    # [rad/s]
+            
+        ### Joint state 
+        joint_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.07, n_max=0.07))      # [rad]
+        joint_vel = ObsTerm(func=mdp.joint_vel_rel, noise=Unoise(n_min=-0.04, n_max=0.04))      # [rad/s]
+
+        def __post_init__(self):
+            self.enable_corruption = True   # IDK
+            self.concatenate_terms = True   # IDK
+
+    policy: PolicyCfg = PolicyCfg()
+
+### REWARDS ###
+
+@configclass
+class RewardsCfg:
+    """
+    Reward configuration for the AlienGo environment.
+    """
+    ## Rewards
+    # Track linear velocity: 0
+    track_lin_vel_xy_exp = RewTerm(
+        func=mdp.track_lin_vel_xy_exp, weight=1.0, params={"command_name": "base_velocity", "std": math.sqrt(0.25)}
+    )
+    # Track angular velocity: 0
+    track_ang_vel_z_exp = RewTerm(
+        func=mdp.track_ang_vel_z_exp, weight=0.9, params={"command_name": "base_velocity", "std": math.sqrt(0.25)}
+    )
+    
+    ## Penalities
+    lin_vel_z_l2    = RewTerm(func=mdp.lin_vel_z_l2,      weight=-0.6)
+    ang_vel_xy_l2   = RewTerm(func=mdp.ang_vel_xy_l2,     weight=-0.05)
+    dof_torques_l2  = RewTerm(func=mdp.joint_torques_l2,  weight=-1.0e-5)
+    dof_acc_l2      = RewTerm(func=mdp.joint_acc_l2,      weight=-2.5e-7)
+    action_rate_l2  = RewTerm(func=mdp.action_rate_l2,    weight=-0.01)
+    
+    dof_pos_dev     = RewTerm(func=mdp.joint_deviation_l1, weight=-0.4) 
+    
+    ## Penalities but with mdp.obs
+    base_height_l2 = RewTerm(
+        func=mdp.base_height_l2,
+        weight=-0.8,
+        params={"asset_cfg": SceneEntityCfg("robot", body_names=["base"]), "target_height": 0.40}, # "target": 0.35         target not a param of base_pos_z
+    )
+    
+    undesired_thigh_contacts = RewTerm(
+        func=mdp.undesired_contacts,
+        weight=-0.6,
+        params={"sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*_thigh"), "threshold": 1.0},
+    )
+    undesired_body_contacts = RewTerm(
+        func=mdp.undesired_contacts,
+        weight=-1.0,
+        params={"sensor_cfg": SceneEntityCfg("contact_forces", body_names="base"), "threshold": 1.0},
+    )
+    
+### Commands ###
+
+@configclass
+class CommandsCfg:
+    """Command terms for the MDP."""   # ASKING TO HAVE 0 Velocity
+
+    base_velocity = mdp.UniformVelocityCommandCfg(
+        asset_name="robot",
+        resampling_time_range=(0.0, 0.0),
+        rel_standing_envs=0.02,
+        rel_heading_envs=1.0,
+        heading_command=True,
+        heading_control_stiffness=0.5,
+        debug_vis=True,
+        ranges=mdp.UniformVelocityCommandCfg.Ranges(
+            lin_vel_x=(0.0, 0.0), lin_vel_y=(0.0, 0.0), ang_vel_z=(0.0, 0.0), heading=(0, 0)
+        ),
+    )
